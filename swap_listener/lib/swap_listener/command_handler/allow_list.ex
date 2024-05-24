b78563725033f0860bfad0359fa27115854ed3dd
José Ribeiro <me@joseribeiro.dev>
2024-05-24 11:33:38 -0300

    add tritium to allowlist

--- swap_listener/lib/swap_listener/command_handler/allow_list.ex
+++ swap_listener/lib/swap_listener/command_handler/allow_list.ex
@@ -1,7 +1,7 @@
 defmodule SwapListener.AllowList do
   @moduledoc false
 
-  @allowed_users ["rpunktj", "burns_unit"]
+  @allowed_users ["rpunktj", "burns_unit", "tritium_vlk"]
 
   def allowed?(username) do
     username in @allowed_users
