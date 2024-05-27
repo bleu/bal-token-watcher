86572883857dec25ba2608e920b29cf12faa921b
José Ribeiro <me@joseribeiro.dev>
2024-05-27 11:52:56 -0300

    fix: point people to balancer app link

--- lib/swap_listener/common/blockchain_configs.ex
+++ lib/swap_listener/common/blockchain_configs.ex
@@ -5,8 +5,8 @@ defmodule SwapListener.Common.BlockchainConfig do
     137 => "Polygon",
     1101 => "PolygonzkEVM",
     42_161 => "Arbitrum",
-    100 => "Optimism",
-    10 => "Gnosis",
+    10 => "Optimism",
+    100 => "Gnosis",
     43_114 => "Avalanche",
     8453 => "Base"
   }
@@ -16,8 +16,8 @@ defmodule SwapListener.Common.BlockchainConfig do
     137 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-polygon-v2",
     1101 => "https://api.studio.thegraph.com/query/24660/balancer-polygon-zk-v2/version/latest",
     42_161 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-arbitrum-v2",
-    100 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-optimism-v2",
-    10 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gnosis-chain-v2",
+    10 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-optimism-v2",
+    100 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gnosis-chain-v2",
     43_114 => "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-avalanche-v2",
     8453 => "https://api.studio.thegraph.com/query/24660/balancer-base-v2/version/latest"
   }
@@ -27,21 +27,21 @@ defmodule SwapListener.Common.BlockchainConfig do
     137 => "https://polygonscan.com",
     1101 => "https://zkevm.polygonscan.com",
     42_161 => "https://arbiscan.io",
-    100 => "https://gnosisscan.io",
     10 => "https://optimistic.etherscan.io",
+    100 => "https://gnosisscan.io",
     43_114 => "https://snowtrace.io",
     8453 => "https://basescan.org"
   }
 
   @balancer_pool_map %{
-    1 => "https://pools.balancer.exchange/#/pool/",
-    137 => "https://pools.balancer.exchange/#/pool/",
-    1101 => "https://pools.balancer.exchange/#/pool/",
-    42_161 => "https://pools.balancer.exchange/#/pool/",
-    100 => "https://pools.balancer.exchange/#/pool/",
-    10 => "https://pools.balancer.exchange/#/pool/",
-    43_114 => "https://pools.balancer.exchange/#/pool/",
-    8453 => "https://pools.balancer.exchange/#/pool/"
+    1 => "https://app.balancer.fi/#/ethereum/pool",
+    137 => "https://app.balancer.fi/#/polygon/pool",
+    1101 => "https://app.balancer.fi/#/zkevm/pool",
+    42_161 => "https://app.balancer.fi/#/arbitrum/pool",
+    10 => "https://app.balancer.fi/#/optimism/pool",
+    100 => "https://app.balancer.fi/#/gnosis-chain/pool",
+    43_114 => "https://app.balancer.fi/#/avalanche/pool/",
+    8453 => "https://app.balancer.fi/#/base/pool"
   }
 
   def subgraph_urls do
